26f8c12f219674c22f30ca6015e9dd4ee1a832a3
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-05-23 21:43:42 -0300

    feat
    insert from styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { SafeAreaView, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const NewText = styled.Text`
+  font-size: 30px;
+  color: red;
+`;
+
 const App = () => {
   return (
     <SafeAreaView>
       <Text>Testando</Text>
+      <NewText>Teste2</NewText>
     </SafeAreaView>
   );
 };
