6804eda52fb6377d7bc4ed708540198b98a2cc8d
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-05-23 21:06:11 -0300

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all', //sempre ter ponto e vírgula
+  singleQuote: true,
+  printWidth: 100, //se tiver mais que 100 caracteres quebra para a linha seguinte
+  tabWidth: 2, //quantidade de linhas para identar
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { SafeAreaView, Text } from 'react-native';
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Testando</Text>
+    </SafeAreaView>
+  );
+};
+export default App;
