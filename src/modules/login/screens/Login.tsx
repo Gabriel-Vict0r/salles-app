db2d72b3c8d766afa8f6e3c47113cd815937e5d8
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-06-24 09:46:21 -0300

    feat
    add from a shared button

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,16 +1,22 @@
 import { Text, TextInput, View } from "react-native";
 import { ContainerLogin } from "../styles/login.style";
 import Input from "../../../shared/components/input/input";
+import Button from "../../../shared/components/button/Button";
+import { ButtonContainer } from "../../../shared/components/button/button.style";
 const Login = () => {
   const style = {
     text: { backgroundColor: "blue" },
     textColor: "white",
+  };
+  const handleOnPress = () => {
+    console.log("clicou");
   };
   return (
     <View>
       <ContainerLogin>
         <Text>Login</Text>
         <Input />
+        <Button margin='8px' title="ENVIAR" onPress={handleOnPress}/>
       </ContainerLogin>
     </View>
   );
---
+++ src/shared/components/button/button.style.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+
+interface ButtonContainerProps {
+  margin?: string;
+}
+
+export const ButtonContainer = styled.TouchableOpacity<ButtonContainerProps>`
+  width: 100%;
+  height: 48px;
+  border-radius: 4px;
+  background-color: blue;
+    ${(props) => (props.margin ? `margin: ${props.margin};` : '')}
+  align-items: center;
+  justify-content: center;
+`;
